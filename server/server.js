73564a68d5f815fb45428c4cfa4f9f5c71115b11
Christoph Schnabl <christoph.schnabl.cs@gmail.com>
2019-06-04 15:51:13 +0200

    Add basic functionality

--- server/server.js
+++ server/server.js
@@ -1,15 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+let clientCount = 0;
+
+let clients = [];
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/client/index.html");
+});
+
+//app.use(express.static(__dirname + "/public"));
 
 function onConnection(socket){
-  socket.on('type', (data) => socket.broadcast.emit('type', data));
+  clientCount++;
+  console.log("Clients connected: ");
+  console.log(clientCount);
+  console.log(clients);
+  
+  socket.on("sendName", function(name) {
+      clients.push(name);
+  });
+
+  socket.on("typing", function(data) {
+    //console.log("text: " + text);
+    io.emit("typing", data);
+
+  });
+
+  socket.on("disconnect", function(){
+    clientCount--;
+    console.log("Clients connected: ");
+    console.log(clientCount);
+  });
+  //socket.on("type", (data) => socket.broadcast.emit("type", data));
 }
 
-io.on('connection', onConnection);
+io.on("connection", onConnection);
 
-http.listen(port, () => console.log('server listening on port ' + port));
+http.listen(port, () => console.log("server listening on port " + port));
