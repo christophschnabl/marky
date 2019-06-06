c06c468d3efb70b7479693ebea4a49ea6b61dba0
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2019-06-06 15:45:39 +0200

    Add user route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,6 @@
-const markdown = require('../modules/markdown.js');
+const { Render } = require('../modules/markdown.js');
 const { Verify } = require('../modules/googleauth.js');
+const { User } = require('../models/user.js');
 const express = require('express'),
       router = express.Router();
 
@@ -10,17 +11,30 @@ const BADREQUEST = {
 
 router.get('/render', function(req, res) {
   if(req.body.text != undefined){
-      res.send(markdown.Render(req.body.text));
+      res.send(Render(req.body.text));
   }else{
     res.status(BADREQUEST.StatusCode);
     res.send(BADREQUEST);
   }
 });
 
-router.post('/verifyid', function(req, res) {
-  if(req.body.id_token != undefined){
-    Verify(req.body.id_token).catch(console.err);
-    res.send("verified");
+router.post('/users', function(req, res) {
+  if(req.body !== undefined){
+    Verify(req.body.id_token).catch(console.err); //TODO: Do something when id is bs
+    const query = {};
+    const update = {
+      id_token: req.body.id_token,
+      name: req.body.name,
+      email: req.body.email,
+      imageUrl: req.body.imageUrl
+    };
+
+    const options = { upsert: true, new: true, setDefaultsOnInsert: true, useFindAndModify: false };
+
+    User.findOneAndUpdate(query, update, options, function(error, doc) {
+        if (error) res.send(error); //TODO: Better error handling
+        res.send(doc);
+    });
   }else{
     res.status(BADREQUEST.StatusCode);
     res.send(BADREQUEST);
