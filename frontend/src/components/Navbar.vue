284c606310db382b276e2d33ead29394f89d7e6c
Christoph Schnabl <christoph.schnabl.cs@gmail.com>
2019-06-09 12:02:36 +0200

    Add navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    wieso geht der scheiss ned
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Navbar",
+        props: {
+
+        },
+        data: () => {
+            return{
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
