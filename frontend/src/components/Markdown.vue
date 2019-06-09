43bd9a4fb060afb18fd5b708f4c4050fe78fadb8
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2019-06-09 20:48:39 +0200

    Add markdown compoenent

---
+++ frontend/src/components/Markdown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="markdown" v-html="value"></div>
+</template>
+
+<script>
+    export default {
+        name: "Markdown",
+        props: {
+            value: String,
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
