19feeb1f41a51bceaf7e18e9c5ca329d977b6733
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2019-06-10 14:01:30 +0200

    Update Document view

--- frontend/src/views/Document.vue
+++ frontend/src/views/Document.vue
@@ -22,6 +22,7 @@
 
 <script>
     import axios from 'axios';
+    import jsPDF from 'jspdf'
     import RandomEmoji from 'node-emoji';
     import Sentencer from 'sentencer';
     import Markdown from '../components/Markdown';
@@ -137,7 +138,11 @@
                 });
             },
             download: function(){
-
+                const doc = new jsPDF();
+                doc.fromHTML(this.document.markdown, 15, 15, {
+                    width: 170
+                });
+                doc.save(this.document.name + ".pdf");
             }
         }
     }
